7e862e7b5c4d58b5787317b1e7397cf2aa1b92e9
Skjalg Teig <skjalgepalg@gmail.com>
2022-11-23 13:46:19 +0100

    chore: Update build config to use babel and packageInfo

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,33 @@
-import buble from '@rollup/plugin-buble'
+import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import { terser } from 'rollup-plugin-terser'
 import serve from 'rollup-plugin-serve'
-import pkg from './package.json'
+import { terser } from 'rollup-plugin-terser'
+import packageInfo from './package.json'
 
-const name = pkg.name.replace('@nrk/', '')
-const banner = `/*! @nrk/${name} v${pkg.version} - Copyright (c) 2019-${new Date().getFullYear()} NRK */`
+const { name, version } = packageInfo
+const shortName = name.replace('@nrk/', '')
+const banner = `/*! ${name} v${version} - Copyright (c) 2019-${new Date().getFullYear()} NRK */`
 const globals = { react: 'React' }
 const external = Object.keys(globals)
 const plugins = [
   resolve(),
   commonjs(),
-  buble(),
+  babel({ presets: [['@babel/preset-env', { modules: false }]], babelHelpers: 'bundled' }),
   Boolean(process.env.ROLLUP_WATCH) && serve('lib')
 ]
 
 export default [{
   input: 'lib/index.js',
   output: [
-    { format: 'cjs', file: `lib/${name}.js`, exports: 'default' },
-    { format: 'esm', file: `lib/${name}.mjs` }
+    { format: 'cjs', file: `lib/${shortName}.js`, exports: 'default' },
+    { format: 'esm', file: `lib/${shortName}.mjs` }
   ],
   plugins,
   external
 }, {
   input: 'lib/index.js',
-  output: { format: 'iife', file: `lib/${name}.min.js`, name: 'customElementToReact', banner, globals },
+  output: { format: 'iife', file: `lib/${shortName}.min.js`, name: 'customElementToReact', banner, globals },
   plugins: plugins.concat(terser({ format: { comments: /^!/ } })),
   external
 }]
